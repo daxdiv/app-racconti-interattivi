5d0be006ea6db055c5df35d173c905105cd234e6
daxdiv <95906740+daxdiv@users.noreply.github.com>
2024-07-31 10:45:54 +0200

    refactor: add hooks folder, move ReactFlow logic inside

--- src/App.tsx
+++ src/App.tsx
@@ -1,81 +1,21 @@
 import "@xyflow/react/dist/style.css";
 
-import {
-  Background,
-  Controls,
-  ReactFlow,
-  addEdge,
-  useEdgesState,
-  useNodesState,
-  useReactFlow,
-  type Node,
-  type Edge,
-  type OnConnect,
-  type OnConnectEnd,
-  type OnConnectStart,
-} from "@xyflow/react";
-import { useCallback, useRef } from "react";
+import { Background, Controls, ReactFlow } from "@xyflow/react";
 
-const initialNodes = [
-  {
-    id: "0",
-    type: "input",
-    data: { label: "Node" },
-    position: { x: 0, y: 50 },
-  },
-];
-
-let id = 1;
-const getId = () => `${id++}`;
+import useReactFlowConnection from "./hooks/useReactFlowConnection";
+import { useRef } from "react";
 
 function App() {
   const reactFlowWrapper = useRef(null);
-  const connectingNodeId = useRef<null | string>(null);
-  const [nodes, setNodes, onNodesChange] = useNodesState<Node>(initialNodes);
-  const [edges, setEdges, onEdgesChange] = useEdgesState<Edge>([]);
-  const { screenToFlowPosition } = useReactFlow();
-  const onConnect: OnConnect = useCallback(params => {
-    // reset the start node on connections
-    connectingNodeId.current = null;
-    setEdges(eds => addEdge(params, eds));
-  }, []);
-
-  const onConnectStart: OnConnectStart = useCallback((_, { nodeId }) => {
-    connectingNodeId.current = nodeId;
-  }, []);
-
-  const onConnectEnd: OnConnectEnd = useCallback(
-    event => {
-      if (!connectingNodeId.current) return;
-      if (!event.target) return;
-
-      const targetIsPane = (event.target as Element).classList.contains(
-        "react-flow__pane"
-      );
-
-      if (targetIsPane) {
-        const id = getId();
-        const newNode: Node = {
-          id,
-          position: screenToFlowPosition({
-            x: (event as MouseEvent).clientX,
-            y: (event as MouseEvent).clientY,
-          }),
-          data: { label: `Node ${id}` },
-          origin: [0.5, 0.0],
-        };
-        const newEgde: Edge = {
-          id,
-          source: connectingNodeId.current,
-          target: id,
-        };
-
-        setNodes(nds => nds.concat(newNode));
-        setEdges(eds => eds.concat(newEgde));
-      }
-    },
-    [screenToFlowPosition]
-  );
+  const {
+    nodes,
+    edges,
+    onNodesChange,
+    onEdgesChange,
+    onConnect,
+    onConnectStart,
+    onConnectEnd,
+  } = useReactFlowConnection();
 
   return (
     <div
---
+++ src/hooks/useReactFlowConnection.ts
@@ -0,0 +1,84 @@
+import {
+  addEdge,
+  useEdgesState,
+  useNodesState,
+  useReactFlow,
+  type Edge,
+  type Node,
+  type OnConnect,
+  type OnConnectEnd,
+  type OnConnectStart,
+} from "@xyflow/react";
+import { useCallback, useRef } from "react";
+
+const REACT_FLOW_PANE_CLASS = "react-flow__pane";
+const INITIAL_NODE = [
+  {
+    id: "0",
+    type: "input",
+    data: { label: "Node" },
+    position: { x: 0, y: 50 },
+  },
+];
+
+let id = 1;
+const incrementId = () => `${id++}`;
+
+export default function useReactFlowConnection() {
+  const connectingNodeId = useRef<null | string>(null);
+  const [nodes, setNodes, onNodesChange] = useNodesState<Node>(INITIAL_NODE);
+  const [edges, setEdges, onEdgesChange] = useEdgesState<Edge>([]);
+  const { screenToFlowPosition } = useReactFlow();
+
+  const onConnect: OnConnect = useCallback(params => {
+    connectingNodeId.current = null;
+    setEdges(eds => addEdge(params, eds));
+  }, []);
+
+  const onConnectStart: OnConnectStart = useCallback((_, { nodeId }) => {
+    connectingNodeId.current = nodeId;
+  }, []);
+
+  const onConnectEnd: OnConnectEnd = useCallback(
+    event => {
+      if (!connectingNodeId.current) return;
+      if (!event.target) return;
+
+      const targetIsPane = (event.target as Element).classList.contains(
+        REACT_FLOW_PANE_CLASS
+      );
+
+      if (!targetIsPane) return;
+
+      const id = incrementId();
+      const newNode: Node = {
+        id,
+        position: screenToFlowPosition({
+          x: (event as MouseEvent).clientX,
+          y: (event as MouseEvent).clientY,
+        }),
+        data: { label: `Node ${id}` },
+        origin: [0.5, 0.0],
+      };
+      const newEgde: Edge = {
+        id,
+        source: connectingNodeId.current,
+        target: id,
+      };
+
+      setNodes(nds => nds.concat(newNode));
+      setEdges(eds => eds.concat(newEgde));
+    },
+    [screenToFlowPosition]
+  );
+
+  return {
+    nodes,
+    edges,
+    onNodesChange,
+    onEdgesChange,
+    onConnect,
+    onConnectStart,
+    onConnectEnd,
+  };
+}
