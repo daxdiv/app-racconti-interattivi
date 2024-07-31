b5b6e6dcaa041c9e97d9fc3d4cb58d64a68a43b2
daxdiv <david200288l@gmail.com>
2024-07-31 10:09:00 +0200

    feat: add ReactFlowProvider component

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import App from "./App.tsx";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ReactFlowProvider } from "@xyflow/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ReactFlowProvider>
+      <App />
+    </ReactFlowProvider>
   </React.StrictMode>
 );
