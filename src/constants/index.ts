08c621ee11d9f48fc4c4e8b92d7b2a5688bfcf5a
daxdiv <95906740+daxdiv@users.noreply.github.com>
2024-08-04 18:39:25 +0200

    feat: add more properties to DoublePageNodeData type and enforcing of new nodes

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,7 @@
+import type { Node } from "@xyflow/react";
+
 export const REACT_FLOW_PANE_CLASS = "react-flow__pane";
-export const INITIAL_NODE = [
+export const INITIAL_NODES: Node<DoublePageNodeData>[] = [
   {
     id: "0",
     type: "doublePage",
@@ -7,6 +9,18 @@ export const INITIAL_NODE = [
       label: "Pagine 1/2",
       leftPageNumber: 1,
       rightPageNumber: 2,
+      backgroundImage: "",
+      pages: [
+        {
+          content: "",
+          position: "TopLeft",
+        },
+        {
+          content: "",
+          position: "TopRight",
+        },
+      ],
+      audio: "",
       deletable: false,
     },
     position: { x: 0, y: 50 },
