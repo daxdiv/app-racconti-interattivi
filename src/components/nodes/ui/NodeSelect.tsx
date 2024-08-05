0b49c0200beaa2c279287396b8f47fbe706e0c75
daxdiv <95906740+daxdiv@users.noreply.github.com>
2024-08-05 16:02:33 +0200

    feat: update page content position based on select value

--- src/components/nodes/ui/NodeSelect.tsx
+++ src/components/nodes/ui/NodeSelect.tsx
@@ -5,10 +5,30 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { useReactFlow, type Node } from "@xyflow/react";
 
-function NodeSelect() {
+type NodeSelectProps = {
+  id: string;
+  pageNumber: number;
+};
+
+function NodeSelect({ id, pageNumber }: NodeSelectProps) {
+  const { getNode } = useReactFlow<Node<DoublePageNodeData>>();
+
   return (
-    <Select>
+    <Select
+      defaultValue="TopLeft"
+      onValueChange={value => {
+        const nodeToUpdate = getNode(id);
+
+        if (!nodeToUpdate) return;
+
+        const nodeIndexToUpdate = pageNumber % 2 === 0 ? 1 : 0;
+
+        nodeToUpdate.data.pages[nodeIndexToUpdate].position =
+          value as PageContentPosition;
+      }}
+    >
       <SelectTrigger className="w-[200px]">
         <SelectValue placeholder="Posizione contenuto" />
       </SelectTrigger>
