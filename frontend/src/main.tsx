5908072e6b19bfbba9685629cb5678d0eed0eeec
daxdiv <david200288l@gmail.com>
2024-08-16 17:10:42 +0200

    feat(frontend): add QueryClientProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,18 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import App from "./App.tsx";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ReactFlowProvider } from "@xyflow/react";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ReactFlowProvider>
-      <App />
-    </ReactFlowProvider>
+    <QueryClientProvider client={queryClient}>
+      <ReactFlowProvider>
+        <App />
+      </ReactFlowProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
