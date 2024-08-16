ecf212a1e5ab853a3bf30b136100321f8a370a38
daxdiv <95906740+daxdiv@users.noreply.github.com>
2024-08-16 17:11:35 +0200

    feat(backend): config cors and body-parser

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,5 @@
+import bodyParser from "body-parser";
+import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 
@@ -6,9 +8,13 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors({ origin: "http://localhost:5173" })); // TODO: set frontend URL inside .env
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app
   .listen(PORT, () => {
-    console.log("Server running at PORT: ", PORT);
+    console.log(`Server running at PORT: ${PORT}`);
   })
   .on("error", error => {
     throw new Error(error.message);
