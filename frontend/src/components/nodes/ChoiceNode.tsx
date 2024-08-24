a78001e4d9b391b4ae67c1dcc9d2ba2d37da6b43
daxdiv <95906740+daxdiv@users.noreply.github.com>
2024-08-24 11:36:59 +0200

    feat(frontend): add unlinked and trash icons, add tooltips, delete choice node on trash icon click

--- frontend/src/components/nodes/ChoiceNode.tsx
+++ frontend/src/components/nodes/ChoiceNode.tsx
@@ -1,4 +1,10 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import {
   Handle,
   Position,
@@ -7,13 +13,36 @@ import {
   type NodeProps,
 } from "@xyflow/react";
 import { Button } from "@/components/ui/button";
-import { Edit } from "lucide-react";
+import { Edit, Trash2, Unlink } from "lucide-react";
+import useNodeUtils from "@/hooks/useNodeUtils";
+import { TOOLTIP_DELAY_DURATION } from "@/constants";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
 type ChoiceNode = Node<ChoiceNodeData>;
 type ChoiceNodeProps = NodeProps<ChoiceNode>;
 
 function ChoiceNode(props: ChoiceNodeProps) {
   const connections = useHandleConnections({ type: "source" });
+  const { isNodeUnlinked, onNodeDelete } = useNodeUtils();
+
+  const { id } = props;
+  const { label } = props.data;
 
   return (
     <>
@@ -22,12 +51,68 @@ function ChoiceNode(props: ChoiceNodeProps) {
         position={Position.Left}
       />
       <Card className="w-[300px] border-[1.5px] border-yellow-500">
-        <CardHeader className="mb-0">
-          <CardTitle className="text-xl">{props.data.label}</CardTitle>
-          {/* <CardDescription>Nodo scelta</CardDescription> */}
+        <CardHeader className="flex flex-row justify-between items-center space-y-0">
+          <div className="flex justify-center items-center gap-x-2">
+            <CardTitle>{label}</CardTitle>
+
+            <CardDescription />
+
+            {isNodeUnlinked(id) && (
+              <TooltipProvider delayDuration={TOOLTIP_DELAY_DURATION}>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Unlink
+                      className="cursor-default text-primary py-1 px-1 rounded-full bg-yellow-500 hover:bg-yellow-500/70 nodrag nopan"
+                      size={24}
+                    />
+                  </TooltipTrigger>
+                  <TooltipContent>Pagine non collegate</TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+            )}
+          </div>
+
+          <div className="flex justify-center items-center gap-x-1">
+            <AlertDialog>
+              <TooltipProvider delayDuration={TOOLTIP_DELAY_DURATION}>
+                <Tooltip>
+                  <AlertDialogTrigger asChild>
+                    <TooltipTrigger asChild>
+                      <Trash2
+                        className="cursor-pointer text-secondary p-1 rounded-full bg-destructive hover:bg-destructive/70 nodrag nopan"
+                        size={24}
+                      />
+                    </TooltipTrigger>
+                  </AlertDialogTrigger>
+                  <TooltipContent>Elimina nodo scelta</TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>Eliminare nodo scelta?</AlertDialogTitle>
+                  <AlertDialogDescription>
+                    Una volta eliminato, dovrai ricrearlo da zero.
+                  </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogAction
+                    onClick={() => {
+                      onNodeDelete(id);
+                    }}
+                    className="bg-destructive hover:bg-destructive/70"
+                  >
+                    Si
+                  </AlertDialogAction>
+                  <AlertDialogCancel className="bg-primary hover:bg-primary/90 text-secondary hover:text-secondary">
+                    No
+                  </AlertDialogCancel>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
+          </div>
         </CardHeader>
 
-        <CardContent className="-mt-4 -mb-2">
+        <CardContent>
           <Button
             variant="outline"
             className="w-full"
