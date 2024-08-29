907fe6478ea34a441e301aed62704cb21b237e84
daxdiv <david200288l@gmail.com>
2024-08-29 11:56:12 +0200

    feat(frontend): show text choice only if provided

--- frontend/src/components/ui/nodes/choice/PreviewChoiceDialog.tsx
+++ frontend/src/components/ui/nodes/choice/PreviewChoiceDialog.tsx
@@ -29,13 +29,15 @@ function PreviewChoiceDialog({ data, trigger }: PreviewChoiceDialogProps) {
 
         <Card>
           <CardContent className="pt-6 justify-center items-center gap-y-3">
-            <p
-              className={cn({
-                "text-muted-foreground": data.text.length === 0,
-              })}
-            >
-              {data.text || "Cosa deve fare Cappuccetto Rosso quando incontra il lupo?"}
-            </p>
+            {data.text && (
+              <p
+                className={cn({
+                  "text-muted-foreground": data.text.length === 0,
+                })}
+              >
+                {data.text || "Cosa deve fare Cappuccetto Rosso quando incontra il lupo?"}
+              </p>
+            )}
 
             <div className="flex justify-center items-center gap-x-5">
               <div className="flex justify-center items-center bg-muted-foreground/65 rounded-full size-28">
