320100eb5f9625f4beaa6f29bd4d625dd3ee7b95
daxdiv <david200288l@gmail.com>
2024-09-25 17:43:30 +0200

    feat(frontend): remove `data` from each node, fix a bug where there can still be cycles, remove `getNodeData` function

--- frontend/src/hooks/useNodeUtils.ts
+++ frontend/src/hooks/useNodeUtils.ts
@@ -2,8 +2,7 @@ import { getIncomers, getOutgoers, useReactFlow, type Node } from "@xyflow/react
 import toast from "react-hot-toast";
 
 function useNodeUtils() {
-  const { getNode, getNodes, getEdges, setNodes, deleteElements, updateNodeData } =
-    useReactFlow<Node<DoublePageNodeData>>();
+  const { getNode, getNodes, getEdges, setNodes, deleteElements } = useReactFlow<Node>();
 
   const onNodeDelete = (id: string) => {
     setNodes(prevNodes => prevNodes.filter(node => node.id !== id));
@@ -20,54 +19,9 @@ function useNodeUtils() {
   const onNodeCreate = (id: number) => {
     const initialNode = getNode("0")!;
 
-    let newNode: Node<DoublePageNodeData>;
-
-    const doublePageNodePreview: DoublePageNodeData["preview"] = {
-      label: "Titolo",
-      leftPageNumber: id + 1,
-      rightPageNumber: id + 2,
-      backgroundImage: new File([], ""),
-      pages: [
-        {
-          text: {
-            content: "",
-            position: "TopLeft",
-          },
-        },
-        {
-          text: {
-            content: "",
-            position: "TopLeft",
-          },
-        },
-      ] as [Page, Page],
-      audio: new File([], ""),
-    };
-
-    const doublePageNodeData: DoublePageNodeData = {
-      label: "Titolo",
-      leftPageNumber: Number(id) + 1,
-      rightPageNumber: id + 2,
-      pages: [
-        {
-          text: {
-            content: "",
-            position: "TopLeft",
-          },
-        },
-        {
-          text: {
-            content: "",
-            position: "TopLeft",
-          },
-        },
-      ] as [Page, Page],
-      preview: doublePageNodePreview,
-    };
-
-    newNode = {
+    const newNode: Node = {
       id: `${id}`,
-      data: doublePageNodeData,
+      data: {},
       position: {
         x: initialNode.position.x - (Math.floor(Math.random() * 8) * 20 + 50),
         y: initialNode.position.y - (Math.floor(Math.random() * 8) * 20 + 50),
@@ -88,77 +42,12 @@ function useNodeUtils() {
 
     return incomers.length === 0 && outgoers.length === 0;
   };
-  const getNodeData = (id: string): DoublePageNodeData => {
-    const node = getNode(id);
-
-    if (!node) throw new Error("Nodo non trovato");
-
-    return node.data;
-  };
-  const resetPreview = (id: string) => {
-    const data = getNodeData(id);
-
-    const defaultQuestionPreview: DoublePageNodeData["preview"]["question"] = {
-      text: data.question?.text || "",
-      audio: [new File([], ""), new File([], ""), new File([], "")],
-      options: data.question?.options || ["", ""],
-      values: data.question?.values || ["", ""],
-      feedback: {
-        list: [
-          {
-            text: data.question?.feedback.list[0].text || "",
-            audio: new File([], ""),
-          },
-          {
-            text: data.question?.feedback.list[1].text || "",
-            audio: new File([], ""),
-          },
-        ],
-        option: data.question?.feedback.option || "",
-      },
-    };
-    const defaultChoicePreview: DoublePageNodeData["preview"]["choice"] = {
-      text: data.choice?.text || "",
-      audio: [new File([], ""), new File([], ""), new File([], "")],
-      options: data.choice?.options || ["", ""],
-      values: data.choice?.values || ["", ""],
-      feedback: {
-        list: [
-          {
-            text: data.choice?.feedback.list[0].text || "",
-            audio: new File([], ""),
-          },
-          {
-            text: data.choice?.feedback.list[1].text || "",
-            audio: new File([], ""),
-          },
-        ],
-        option: data.choice?.feedback.option || "",
-      },
-      nextSteps: data.choice?.nextSteps || [-1, -1],
-    };
-
-    updateNodeData(id, {
-      preview: {
-        label: data.label,
-        leftPageNumber: data.leftPageNumber,
-        rightPageNumber: data.rightPageNumber,
-        backgroundImage: new File([], ""),
-        pages: data.pages,
-        audio: new File([], ""),
-        question: data.question ? defaultQuestionPreview : undefined,
-        choice: data.choice ? defaultChoicePreview : undefined,
-      },
-    });
-  };
 
   return {
     onNodeDelete,
     onNodeCreate,
     isNodeUnlinked,
     getNode,
-    getNodeData,
-    resetPreview,
   };
 }
 
