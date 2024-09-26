4d9a0626b64720f51ffc7019d0e694da372f6471
daxdiv <95906740+daxdiv@users.noreply.github.com>
2024-09-26 21:43:33 +0200

    feat(frontend): add object to form data utility function

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -12,3 +12,25 @@ export function equalObjects<T extends Record<string, unknown>>(obj1: T, obj2: T
 export function truncate(str: string, maxLen: number) {
   return str.length > maxLen ? `${str.slice(0, maxLen - 1)}...` : str;
 }
+
+export function objectToFormData(
+  obj: Record<string, unknown>,
+  form?: FormData,
+  namespace?: string
+): FormData {
+  const formData = form || new FormData();
+
+  for (const property in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, property)) {
+      const formKey = namespace ? `${namespace}[${property}]` : property;
+
+      if (typeof obj[property] === "object" && !(obj[property] instanceof File)) {
+        objectToFormData(obj[property] as Record<string, unknown>, formData, formKey);
+      } else {
+        formData.append(formKey, obj[property] as string);
+      }
+    }
+  }
+
+  return formData;
+}
