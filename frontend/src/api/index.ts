c1dfd191220f61ba4ab6eb9cffc2d51f4c34c7a5
daxdiv <95906740+daxdiv@users.noreply.github.com>
2024-09-27 12:23:21 +0200

    refactor(frontend): remove `api/node` folder

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -2,12 +2,15 @@ import { PageSchema } from "@/lib/zod";
 import { objectToFormData } from "@/lib/utils";
 
 export async function saveNode(id: string, params: PageSchema) {
-  const formData = objectToFormData({ id, ...params });
+  const formData = objectToFormData({ nodeId: id, ...params });
 
-  const response = await fetch(`${import.meta.env.VITE_SERVER_URL}/node`, {
-    method: "POST",
-    body: formData,
-  });
+  const response = await fetch(
+    `${import.meta.env.VITE_SERVER_URL}/node?nodeType=${params.type}`,
+    {
+      method: "POST",
+      body: formData,
+    }
+  );
 
   if (response.status !== 201) {
     const error = await response.json();
