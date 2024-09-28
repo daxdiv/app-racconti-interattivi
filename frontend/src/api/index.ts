1974a789eb434228490ffdd3892b73b20135852e
daxdiv <david200288l@gmail.com>
2024-09-28 17:46:49 +0200

    feat(frontend): add api endpoint to retrieve node data

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,7 +1,7 @@
 import { PageSchema } from "@/lib/zod";
 import { objectToFormData } from "@/lib/utils";
 
-export async function saveNode(id: string, params: PageSchema) {
+export async function savePageNode(id: string, params: PageSchema) {
   const formData = objectToFormData({ nodeId: id, ...params });
 
   const response = await fetch(
@@ -13,6 +13,18 @@ export async function saveNode(id: string, params: PageSchema) {
   );
 
   if (response.status !== 201) {
+    const error = await response.json();
+
+    throw new Error(error.message);
+  }
+
+  return await response.json();
+}
+
+export async function getPageNode(id: string) {
+  const response = await fetch(`${import.meta.env.VITE_SERVER_URL}/node/${id}`);
+
+  if (!response.ok) {
     const error = await response.json();
 
     throw new Error(error.message);
