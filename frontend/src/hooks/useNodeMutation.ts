62e355219f2dd7b9a3c3507cc9fd97618ec22876
daxdiv <95906740+daxdiv@users.noreply.github.com>
2024-09-29 20:52:28 +0200

    feat(frontend): invalidate get node query on mutation success

--- frontend/src/hooks/useNodeMutation.ts
+++ frontend/src/hooks/useNodeMutation.ts
@@ -1,14 +1,16 @@
 import { savePageNode } from "@/api";
 import { PageSchema } from "@/lib/zod";
-import { type DefaultError, useMutation } from "@tanstack/react-query";
+import { type DefaultError, useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 
 function useNodeMutation(id: string, onSuccess: () => void) {
+  const queryClient = useQueryClient();
   const saveNode = useMutation<unknown, DefaultError, PageSchema>({
     mutationKey: ["save-node", id],
     mutationFn: data => savePageNode(id, data),
     onSuccess() {
       onSuccess();
+      queryClient.invalidateQueries({ queryKey: ["get-node"] });
     },
     onError(error) {
       toast.error(error.message);
