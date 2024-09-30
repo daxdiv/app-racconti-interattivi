8d87aa09a7dae33ce3a183f4c627a5a15cb70fb1
daxdiv <david200288l@gmail.com>
2024-09-30 21:34:55 +0200

    refactor(backend): use express json middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -23,7 +23,7 @@ import pageNodeRouter from "./routers/pageNode.router";
   const PORT = process.env.PORT || 3000;
 
   app.use(cors({ origin: process.env.CLIENT_URL }));
-  app.use(bodyParser.json());
+  app.use(express.json());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(express.static("public"));
   app.use("/node", pageNodeRouter);
