806719775000a16e9958de1d70f7fb07db40c532
daxdiv <david200288l@gmail.com>
2024-09-30 09:24:17 +0200

    feat(frontend): in preview components get data from the form to show the user the changes made

--- frontend/src/components/ui/nodes/doublePage/preview/questionChoiceTab/Feedbacks.tsx
+++ frontend/src/components/ui/nodes/doublePage/preview/questionChoiceTab/Feedbacks.tsx
@@ -8,8 +8,8 @@ type FeedbacksProps = {
 function Feedbacks({ field }: FeedbacksProps) {
   const { data } = useNodeQueryContext();
   const form = useFormContext();
-  const formFirstFeedbackAudio = form.watch("feedbacks.list.0.audio");
-  const formSecondFeedbackAudio = form.watch("feedbacks.list.0.audio");
+  const formFirstFeedbackAudio = form.watch("feedback.list.0.audio");
+  const formSecondFeedbackAudio = form.watch("feedback.list.0.audio");
   const firstFeedbackAudio =
     formFirstFeedbackAudio && formFirstFeedbackAudio.size > 0
       ? URL.createObjectURL(formFirstFeedbackAudio)
@@ -23,12 +23,12 @@ function Feedbacks({ field }: FeedbacksProps) {
     <div className="flex flex-col justify-center items-center gap-x-5 mt-2">
       <div className="flex-col">
         <p className="font-extrabold">
-          Feedback scegliendo "{data?.[field]?.options[0]}"
+          Feedback scegliendo "{form.getValues(`${field}.options.0`)}"
         </p>
 
         <div className="flex flex-col justify-center items-center">
           <p className="flex justify-center items-center gap-x-2 text-sm break-words">
-            {data?.feedback?.list[0].text}
+            {form.getValues("feedback.list.0.text")}
 
             {firstFeedbackAudio ? (
               <audio
@@ -53,12 +53,13 @@ function Feedbacks({ field }: FeedbacksProps) {
       </div>
       <div className="flex-col mt-3">
         <p className="font-extrabold">
-          Feedback scegliendo "{data?.[field]?.options[1]}"
+          Feedback scegliendo "{form.getValues(`${field}.options.1`)}"
         </p>
 
         <div className="flex flex-col justify-center items-center">
           <p className="flex justify-center items-center gap-x-2 text-sm break-words">
             {data?.feedback?.list[1].text}
+            {form.getValues("feedback.list.1.text")}
 
             {secondFeedbackAudio ? (
               <audio
--- frontend/src/components/ui/nodes/doublePage/preview/questionChoiceTab/Values.tsx
+++ frontend/src/components/ui/nodes/doublePage/preview/questionChoiceTab/Values.tsx
@@ -1,15 +1,17 @@
-import { useNodeQueryContext } from "@/hooks/useNodeQueryContext";
+import { useFormContext } from "react-hook-form";
 
 function Values() {
-  const { data } = useNodeQueryContext();
+  const form = useFormContext();
 
   return (
     <div className="mt-3 flex flex-col justify-center items-center gap-x-2">
       <div className="flex justify-center items-center">
-        <span className="font-extrabold">Primo valore: </span> {data?.values?.[0]}
+        <span className="font-extrabold">Primo valore: </span>{" "}
+        {form.getValues("values.0")}
       </div>
       <div className="flex justify-center items-center">
-        <span className="font-extrabold">Secondo valore: </span> {data?.values?.[1]}
+        <span className="font-extrabold">Secondo valore: </span>{" "}
+        {form.getValues("values.1")}
       </div>
     </div>
   );
