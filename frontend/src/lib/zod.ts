857ff350bc9fb719f04da0b8ee85bab036b65ee2
daxdiv <95906740+daxdiv@users.noreply.github.com>
2024-09-30 23:45:11 +0200

    refactor(backend): more robust schema

--- frontend/src/lib/zod.ts
+++ frontend/src/lib/zod.ts
@@ -9,8 +9,40 @@ import {
 
 import { z } from "zod";
 
-export const pageSchema = z.object({
-  type: z.enum(["base", "question", "choice"]),
+const baseSchema = z.object({
+  type: z.literal("base"),
+  label: z.string().min(1, { message: "Il titolo non può essere vuoto" }),
+  pages: z
+    .array(
+      z.object({
+        text: z.object({
+          content: z
+            .string()
+            .min(1, { message: "Il contenuto non può essere vuoto" })
+            .max(MAX_TEXT_CONTENT_LENGTH, {
+              message: `Il contenuto può essere al massimo di ${MAX_TEXT_CONTENT_LENGTH} caratteri`,
+            })
+            .default(""),
+          position: z.custom<PageTextPosition>(),
+          class: z.string().default("").optional(),
+        }),
+      })
+    )
+    .min(2)
+    .max(2),
+  background: z
+    .instanceof(File)
+    .refine(file => file.size > 0, "Inserire un file")
+    .refine(file => file.size < MAX_FILE_SIZE, "File troppo grande")
+    .or(z.string().url()),
+  audio: z
+    .instanceof(File)
+    .refine(file => file.size > 0, "Inserire un file")
+    .refine(file => file.size < MAX_FILE_SIZE, "File troppo grande")
+    .or(z.string().url()),
+});
+const questionSchema = z.object({
+  type: z.literal("question"),
   label: z.string().min(1, { message: "Il titolo non può essere vuoto" }),
   pages: z
     .array(
@@ -73,38 +105,6 @@ export const pageSchema = z.object({
         .max(2),
     })
     .optional(),
-  choice: z
-    .object({
-      text: z
-        .string()
-        .min(1, { message: "Il testo di una scelta non può essere vuoto" })
-        .max(MAX_QUESTION_CHOICE_TEXT_LENGTH, {
-          message: `Il testo di una scelta può essere al massimo ${MAX_QUESTION_CHOICE_TEXT_LENGTH} caratteri`,
-        })
-        .default(""),
-      audio: z
-        .array(
-          z
-            .instanceof(File)
-            .refine(file => file.size > 0, "Inserire un file")
-            .refine(file => file.size < MAX_FILE_SIZE, "File troppo grande")
-            .or(z.string().url())
-        )
-        .max(3),
-      options: z
-        .array(
-          z
-            .string()
-            .min(1, { message: "L'opzione non può essere vuota" })
-            .max(MAX_OPTION_LENGTH, {
-              message: `L'opzione può essere al massimo ${MAX_OPTION_LENGTH} caratteri`,
-            })
-            .default("")
-        )
-        .min(2)
-        .max(2),
-    })
-    .optional(),
   values: z
     .array(
       z
@@ -115,8 +115,8 @@ export const pageSchema = z.object({
         })
         .default("")
     )
-    // .min(2)
-    // .max(2)
+    .min(2)
+    .max(2)
     .optional(),
   feedback: z
     .object({
@@ -143,4 +143,108 @@ export const pageSchema = z.object({
     })
     .optional(),
 });
+const choiceSchema = z.object({
+  type: z.literal("choice"),
+  label: z.string().min(1, { message: "Il titolo non può essere vuoto" }),
+  pages: z
+    .array(
+      z.object({
+        text: z.object({
+          content: z
+            .string()
+            .min(1, { message: "Il contenuto non può essere vuoto" })
+            .max(MAX_TEXT_CONTENT_LENGTH, {
+              message: `Il contenuto può essere al massimo di ${MAX_TEXT_CONTENT_LENGTH} caratteri`,
+            })
+            .default(""),
+          position: z.custom<PageTextPosition>(),
+          class: z.string().default("").optional(),
+        }),
+      })
+    )
+    .min(2)
+    .max(2),
+  background: z
+    .instanceof(File)
+    .refine(file => file.size > 0, "Inserire un file")
+    .refine(file => file.size < MAX_FILE_SIZE, "File troppo grande")
+    .or(z.string().url()),
+  audio: z
+    .instanceof(File)
+    .refine(file => file.size > 0, "Inserire un file")
+    .refine(file => file.size < MAX_FILE_SIZE, "File troppo grande")
+    .or(z.string().url()),
+  choice: z
+    .object({
+      text: z
+        .string()
+        .min(1, { message: "Il testo di una scelta non può essere vuoto" })
+        .max(MAX_QUESTION_CHOICE_TEXT_LENGTH, {
+          message: `Il testo di una scelta può essere al massimo ${MAX_QUESTION_CHOICE_TEXT_LENGTH} caratteri`,
+        })
+        .default(""),
+      audio: z
+        .array(
+          z
+            .instanceof(File)
+            .refine(file => file.size > 0, "Inserire un file")
+            .refine(file => file.size < MAX_FILE_SIZE, "File troppo grande")
+            .or(z.string().url())
+        )
+        .min(3)
+        .max(3),
+      options: z
+        .array(
+          z
+            .string()
+            .min(1, { message: "L'opzione non può essere vuota" })
+            .max(MAX_OPTION_LENGTH, {
+              message: `L'opzione può essere al massimo ${MAX_OPTION_LENGTH} caratteri`,
+            })
+            .default("")
+        )
+        .min(2)
+        .max(2),
+    })
+    .optional(),
+  values: z
+    .array(
+      z
+        .string()
+        .min(1, "Il valore non può essere vuoto")
+        .max(MAX_VALUE_LENGTH, {
+          message: `Il valore può essere al massimo ${MAX_VALUE_LENGTH} caratteri`,
+        })
+        .default("")
+    )
+    .min(2)
+    .max(2)
+    .optional(),
+  feedback: z
+    .object({
+      list: z
+        .array(
+          z.object({
+            text: z.string().min(1, "Il feedback non può essere vuoto"),
+            audio: z
+              .instanceof(File)
+              .refine(file => file.size > 0, "Inserire un file")
+              .refine(file => file.size < MAX_FILE_SIZE, "File troppo grande")
+              .or(z.string().url()),
+          })
+        )
+        .min(2)
+        .max(2),
+      option: z
+        .string()
+        .min(1, "L'opzione non può essere vuota")
+        .max(MAX_CONTINUE_OPTION_LENGTH, {
+          message: `L'opzione per continuare può essere al massimo ${MAX_CONTINUE_OPTION_LENGTH} caratteri`,
+        })
+        .default(""),
+    })
+    .optional(),
+});
+
+export const pageSchema = baseSchema.or(questionSchema).or(choiceSchema);
 export type PageSchema = z.infer<typeof pageSchema>;
