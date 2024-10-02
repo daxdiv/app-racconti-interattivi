fdbe2bce4e4fb7943776c55a6556e28e3b0d90df
daxdiv <david200288l@gmail.com>
2024-10-02 10:37:44 +0200

    feat(backend): add `writeFiles` utility function

--- backend/src/lib/utils.ts
+++ backend/src/lib/utils.ts
@@ -1,16 +1,40 @@
+import fs from "fs";
 import sharp from "sharp";
 
 export async function splitImage(
-  path: string,
+  buffer: Buffer | string,
   firstHalfPath: string,
   secondHalfPath: string
 ) {
-  const { width, height } = await sharp(path).metadata();
+  const { width, height } = await sharp(buffer).metadata();
   const halfWidth = Math.floor(width! / 2);
-  await sharp(path)
+
+  await sharp(buffer)
     .extract({ width: halfWidth, height: height!, left: 0, top: 0 })
     .toFile(firstHalfPath);
-  await sharp(path)
+  await sharp(buffer)
     .extract({ width: halfWidth, height: height!, left: halfWidth, top: 0 })
     .toFile(secondHalfPath);
+}
+
+export function writeFiles(files: Express.Multer.File[]) {
+  files.map(async f => {
+    const id = f.originalname.split("_")[0];
+    const destPath = `public/${id}`;
+
+    if (!fs.existsSync(destPath)) {
+      fs.mkdirSync(destPath, { recursive: true });
+    }
+
+    fs.writeFile(`${destPath}/${f.originalname}`, f.buffer, error => {
+      if (error) console.error(error);
+    });
+
+    if (f.originalname.endsWith("background"))
+      await splitImage(
+        f.buffer,
+        `${destPath}/${id}_background_left`,
+        `${destPath}/${id}_background_right`
+      );
+  });
 }
