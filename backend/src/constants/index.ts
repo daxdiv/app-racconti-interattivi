d424c71866b5fd5ce1248c39a31b7b8027e3c842
daxdiv <david231116@icloud.com>
2024-10-03 19:53:42 +0200

    feat(backend): add constants for username/password validation and jwt max age

--- backend/src/constants/index.ts
+++ backend/src/constants/index.ts
@@ -4,3 +4,6 @@ export const MAX_QUESTION_CHOICE_TEXT_LENGTH = 20;
 export const MAX_OPTION_LENGTH = 15;
 export const MAX_VALUE_LENGTH = 15;
 export const MAX_CONTINUE_OPTION_LENGTH = 15;
+export const MIN_USERNAME_LENGTH = 5;
+export const MIN_PASSWORD_LENGTH = 8;
+export const JWT_EXPIRES_IN = 24 * 60 * 60 * 1000; // NOTE: 1 day
