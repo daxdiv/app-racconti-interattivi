507867673c18627442ddfa35696719291f8ed78d
daxdiv <95906740+daxdiv@users.noreply.github.com>
2024-10-03 19:56:59 +0200

    feat(backend): throw is `JWT_SECRET` is not set, use cookie parser and set `credentials`  to true in cors options

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,11 @@
 import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import flowRouter from "./routers/flow.router";
 import mongoose from "mongoose";
+import userRouter from "./routers/user.router";
 
 (async () => {
   dotenv.config();
@@ -13,6 +15,9 @@ import mongoose from "mongoose";
   }
   if (!process.env.DATABASE_URL) {
     throw new Error("Missing DATABASE_URL inside .env");
+  }
+  if (!process.env.JWT_SECRET) {
+    throw new Error("Missing JWT_SECRET inside .env");
   }
 
   await mongoose.connect(process.env.DATABASE_URL);
@@ -25,11 +30,13 @@ import mongoose from "mongoose";
   const app = express();
   const PORT = process.env.PORT || 3000;
 
-  app.use(cors({ origin: process.env.CLIENT_URL }));
+  app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
   app.use(express.json());
+  app.use(cookieParser());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(express.static("public"));
   app.use("/flow", flowRouter);
+  app.use("/user", userRouter);
 
   app
     .listen(PORT, () => {
