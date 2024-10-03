e6bf3b3857278527820881631c99db436dfa64ee
daxdiv <95906740+daxdiv@users.noreply.github.com>
2024-10-03 19:59:10 +0200

    feat(frontend): add constants for username/password validation

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -37,5 +37,7 @@ export const MAX_QUESTION_CHOICE_TEXT_LENGTH = 20;
 export const MAX_OPTION_LENGTH = 15;
 export const MAX_VALUE_LENGTH = 15;
 export const MAX_CONTINUE_OPTION_LENGTH = 15;
+export const MIN_USERNAME_LENGTH = 5;
+export const MIN_PASSWORD_LENGTH = 8;
 export const DEFAULT_BACKGROUND_URL =
   "https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80";
