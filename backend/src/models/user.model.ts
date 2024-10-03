1ca5e0919b68ee34e60e23750f0621bb447bf12d
daxdiv <95906740+daxdiv@users.noreply.github.com>
2024-10-03 19:56:01 +0200

    feat(backend): add user schema, model and router

---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,41 @@
+import mongoose, { Schema } from "mongoose";
+
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+}).index({ username: 1 });
+userSchema.pre("save", async function (next) {
+  try {
+    const hashedPassword = await bcrypt.hash(this.password, 10);
+
+    this.password = hashedPassword;
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+userSchema.methods.comparePassword = async function (password: string) {
+  return await bcrypt.compare(password, this.password, err => {
+    if (err) throw new Error(err.message);
+  });
+};
+userSchema.methods.toJSON = function () {
+  const user = this.toObject();
+
+  delete user.password;
+  delete user.__v;
+
+  return user;
+};
+
+export default mongoose.model("user", userSchema);
