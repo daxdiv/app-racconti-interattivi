6b11852c485a081ae4c4e0505af16731ff89f18e
daxdiv <david231116@icloud.com>
2024-10-04 09:06:49 +0200

    feat(frontend): add `Protected` HOC

---
+++ frontend/src/components/Protected.tsx
@@ -0,0 +1,29 @@
+import { LoaderCircle } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+import useUser from "@/hooks/useUser";
+
+type ProtectedProps = {
+  children: React.ReactNode;
+};
+
+function Protected({ children }: ProtectedProps) {
+  const navigate = useNavigate();
+  const { me } = useUser();
+
+  if (me.isLoading) {
+    return (
+      <div className="flex justify-center items-center min-h-screen">
+        <LoaderCircle className="animate-spin text-white" />
+      </div>
+    );
+  }
+  if (me.isError) {
+    navigate(`/?error=${me.error.message}`);
+
+    return null;
+  }
+
+  return children;
+}
+
+export default Protected;
