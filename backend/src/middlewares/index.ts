3ec647fcfa1ca75e0d2465cf120ce614a7c19ae9
daxdiv <david231116@icloud.com>
2024-10-04 11:55:28 +0200

    feat(backend): add auth middleware, change response message language to italian

---
+++ backend/src/middlewares/index.ts
@@ -0,0 +1,22 @@
+import type { NextFunction, Request, Response } from "express";
+import jwt, { type JwtPayload } from "jsonwebtoken";
+
+export function auth(req: Request, res: Response, next: NextFunction) {
+  const cookie = req.cookies["user"];
+
+  if (!cookie) {
+    res.status(401).json({ message: "Not authenticated" });
+    return;
+  }
+
+  const verified = jwt.verify(cookie, process.env.JWT_SECRET!) as JwtPayload;
+
+  if (!verified) {
+    res.status(401).json({ message: "Not authenticated" });
+    return;
+  }
+
+  res.locals.verified = verified;
+
+  next();
+}
