b521df7a0773236943afb96404cfad5b29153e91
daxdiv <david231116@icloud.com>
2024-10-04 14:41:31 +0200

    style(backend): change error message language to italian

--- backend/src/middlewares/index.ts
+++ backend/src/middlewares/index.ts
@@ -5,14 +5,14 @@ export function auth(req: Request, res: Response, next: NextFunction) {
   const cookie = req.cookies["user"];
 
   if (!cookie) {
-    res.status(401).json({ message: "Not authenticated" });
+    res.status(401).json({ message: "Devi prima effettuare l'accesso" });
     return;
   }
 
   const verified = jwt.verify(cookie, process.env.JWT_SECRET!) as JwtPayload;
 
   if (!verified) {
-    res.status(401).json({ message: "Not authenticated" });
+    res.status(401).json({ message: "Devi prima effettuare l'accesso" });
     return;
   }
 
