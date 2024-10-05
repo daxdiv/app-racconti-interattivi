0386125b84d8e373437c25597325084d338c8084
daxdiv <david231116@icloud.com>
2024-10-05 18:34:33 +0200

    feat(frontend): add hooks and api function to change user info and delete account

--- frontend/src/hooks/useUser.ts
+++ frontend/src/hooks/useUser.ts
@@ -1,20 +1,30 @@
+import type { AuthSchema, PasswordSchema, UsernameSchema } from "@/lib/zod";
 import {
+  deleteAccount as deleteAccountApi,
   me as meApi,
+  setPassword as setPasswordApi,
+  setUsername as setUsernameApi,
   signIn as signInApi,
   signOut as signOutApi,
   signUp as signUpApi,
 } from "@/api";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
-import { AuthSchema } from "@/lib/zod";
-
-type Data = {
+type Message = {
+  message: string;
+};
+export type Data = {
   _id: string;
   username: AuthSchema["username"];
 };
+type SetUsernameVars = UsernameSchema & { userId: string };
+type SetPasswordVars = Pick<PasswordSchema, "password" | "newPassword"> & {
+  userId: string;
+};
 
 function useUser() {
-  const me = useQuery<unknown, { message: string }, Data>({
+  const queryClient = useQueryClient();
+  const me = useQuery<unknown, Message, Data>({
     queryKey: ["me"],
     queryFn: meApi,
     refetchOnMount: false,
@@ -22,20 +32,47 @@ function useUser() {
     retry: false,
     refetchOnWindowFocus: false,
   });
-  const signUp = useMutation<Data, { message: string }, AuthSchema>({
+  const signUp = useMutation<Data, Message, AuthSchema>({
     mutationKey: ["sign-up"],
-    mutationFn: user => signUpApi(user),
+    mutationFn: signUpApi,
   });
-  const signIn = useMutation<{ message: string }, { message: string }, AuthSchema>({
+  const signIn = useMutation<Message, Message, AuthSchema>({
     mutationKey: ["sign-in"],
-    mutationFn: user => signInApi(user),
+    mutationFn: signInApi,
+    onSuccess() {
+      queryClient.refetchQueries({
+        queryKey: ["me"],
+      });
+    },
   });
   const signOut = useMutation({
     mutationKey: ["sign-out"],
     mutationFn: signOutApi,
+    onSuccess() {
+      queryClient.resetQueries({
+        queryKey: ["me"],
+      });
+    },
+  });
+  const setUsername = useMutation<Message, Message, SetUsernameVars>({
+    mutationKey: ["set-username"],
+    mutationFn: setUsernameApi,
+  });
+  const setPassword = useMutation<Message, Message, SetPasswordVars>({
+    mutationKey: ["set-password"],
+    mutationFn: setPasswordApi,
+  });
+  const deleteAccount = useMutation<Message, Message, string>({
+    mutationKey: ["delete-account"],
+    mutationFn: deleteAccountApi,
+    onSuccess() {
+      queryClient.resetQueries({
+        queryKey: ["me"],
+      });
+    },
   });
 
-  return { me, signUp, signIn, signOut };
+  return { me, signUp, signIn, signOut, setUsername, setPassword, deleteAccount };
 }
 
 export default useUser;
