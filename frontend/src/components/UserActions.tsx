11e124b631df2c3b093da7d76cc7aceadd174b71
daxdiv <95906740+daxdiv@users.noreply.github.com>
2024-10-05 18:37:06 +0200

    feat(frontend: add `UserActions` component

---
+++ frontend/src/components/UserActions.tsx
@@ -0,0 +1,33 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import { Button } from "@/components/ui/button";
+import { User } from "lucide-react";
+
+type UserActionsProps = {
+  renderItems: () => React.ReactNode | React.ReactNode[];
+};
+
+function UserActions({ renderItems }: UserActionsProps) {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button size="icon">
+          <User />
+          <span className="sr-only">User actions</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        align="end"
+        className="w-48"
+      >
+        {renderItems()}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+
+export default UserActions;
