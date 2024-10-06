2284aecb7f9f5b38068d4ee646bbe23df7cab213
daxdiv <95906740+daxdiv@users.noreply.github.com>
2024-10-06 09:25:03 +0200

    feat(style): add classes for button if variant is `destructive`

--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -11,7 +11,8 @@ const buttonVariants = cva(
       variant: {
         default:
           "bg-primary text-primary-foreground hover:bg-primary/90 dark:bg-secondary dark:text-primary dark:hover:bg-secondary/70",
-        destructive: "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+        destructive:
+          "bg-destructive text-destructive-foreground hover:bg-destructive/90 dark:text-primary hover:dark:bg-destructive-foreground",
         outline:
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         "destructive-outline":
