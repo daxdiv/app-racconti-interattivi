07ea78552bb36a75bda36ee70e5e035a4e511682
daxdiv <95906740+daxdiv@users.noreply.github.com>
2024-10-06 21:14:33 +0200

    feat(backend): change put flow schema, remove `flowsId` from `User` model, add routes to get flow data, create new flow and update an existing flow

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -13,11 +13,6 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
-    flowsId: {
-      type: [Schema.Types.ObjectId],
-      ref: "flow",
-      default: [],
-    },
   },
   { timestamps: true }
 );
