5d8a5ad0fd4907da7a6948b39f93df1736fce3cb
daxdiv <david231116@icloud.com>
2024-10-14 09:42:32 +0200

    feat(backend): add function to sort node based on flow edges, share media between first and last node

--- backend/src/lib/utils.ts
+++ backend/src/lib/utils.ts
@@ -1,5 +1,7 @@
-import type { NodeSchema } from "./zod";
+import type { EdgeSchema, NodeSchema } from "./zod";
+
 import fs from "fs";
+import path from "path";
 import sharp from "sharp";
 
 export async function splitImage(
@@ -31,12 +33,30 @@ export function writeFiles(userId: string, flowId: string, files: Express.Multer
       if (error) console.error(error);
     });
 
-    if (f.originalname.endsWith("background"))
+    if (f.originalname.endsWith("background")) {
       await splitImage(
         f.buffer,
         `${destPath}/${id}_background_left`,
         `${destPath}/${id}_background_right`
       );
+
+      if (!fs.existsSync(`public/${userId}/${flowId}/end`)) {
+        fs.mkdirSync(`public/${userId}/${flowId}/end`, { recursive: true });
+
+        const files = fs.readdirSync(destPath);
+        const filesToCopy = files.filter(file => file.startsWith("0_"));
+
+        for (const file of filesToCopy) {
+          const sourcePath = path.join(`public/${userId}/${flowId}/0`, file);
+          const destPath = path.join(
+            `public/${userId}/${flowId}/end`,
+            file.replace("0_", "end_")
+          );
+
+          fs.copyFileSync(sourcePath, destPath);
+        }
+      }
+    }
   });
 }
 
@@ -111,4 +131,72 @@ export function createNodesPayload(
   });
 
   return nodes;
+}
+
+export function getSortedNodes(nodes: NodeSchema[], edges: EdgeSchema[]) {
+  const incomingEdges: Map<string, number> = new Map(nodes.map(node => [node.id, 0]));
+  const adjacencyList: Map<string, string[]> = new Map(nodes.map(node => [node.id, []]));
+
+  edges.forEach(edge => {
+    adjacencyList.get(edge.source)?.push(edge.target);
+    incomingEdges.set(edge.target, (incomingEdges.get(edge.target) || 0) + 1);
+  });
+
+  let sortedNodes: NodeSchema[] = [];
+  let queue: NodeSchema[] = [...nodes.filter(node => incomingEdges.get(node.id) === 0)];
+
+  while (queue.length > 0) {
+    const node = queue.shift();
+    if (node) {
+      sortedNodes.push(node);
+
+      adjacencyList.get(node.id)?.forEach(target => {
+        incomingEdges.set(target, (incomingEdges.get(target) || 0) - 1);
+        if (incomingEdges.get(target) === 0) {
+          const targetNode = nodes.find(n => n.id === target);
+          if (targetNode) {
+            queue.push(targetNode);
+          }
+        }
+      });
+    }
+  }
+
+  return sortedNodes.map(n => {
+    const commonFields: Record<string, unknown> = {
+      id: n.id,
+      pages: n.pages,
+      audio: n.audio,
+    };
+
+    if (n.id === "end") {
+      commonFields.lastPage = true;
+      commonFields.evaluation = {
+        show: true,
+        label: "Quanto ti è piaciuta la storia?",
+      };
+    }
+
+    switch (n.type) {
+      case "base":
+        return commonFields;
+      case "question":
+        return {
+          ...commonFields,
+          question: n.question,
+          values: n.values,
+          feedback: n.feedback,
+        };
+      case "choice":
+        return {
+          ...commonFields,
+          choice: n.choice,
+          values: n.values,
+          feedback: n.feedback,
+          nextSteps: n.nextSteps,
+        };
+      default:
+        return commonFields;
+    }
+  });
 }
