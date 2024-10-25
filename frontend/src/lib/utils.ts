9a25a7d099f70b74f4d0a1f744728aa05fce469c
daxdiv <95906740+daxdiv@users.noreply.github.com>
2024-10-25 14:55:23 +0200

    refactor(frontend): remove unused function

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -5,10 +5,6 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function equalObjects<T extends Record<string, unknown>>(obj1: T, obj2: T) {
-  return JSON.stringify(obj1) === JSON.stringify(obj2);
-}
-
 export function truncate(str: string, maxLen: number) {
   return str.length > maxLen ? `${str.slice(0, maxLen - 1)}...` : str;
 }
