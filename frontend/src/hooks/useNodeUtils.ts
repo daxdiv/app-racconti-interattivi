d94e0373e94a1014a57d29b7dc3e21997703dc14
daxdiv <david231116@icloud.com>
2024-10-26 13:47:22 +0200

    feat(frontend): save flow state when create/delete a node

--- frontend/src/hooks/useNodeUtils.ts
+++ frontend/src/hooks/useNodeUtils.ts
@@ -1,12 +1,35 @@
 import { getIncomers, getOutgoers, useReactFlow, type Node } from "@xyflow/react";
 import toast from "react-hot-toast";
 import { DEFAULT_DATA } from "@/constants";
+import useFlow from "./useFlow";
+import { useParams } from "react-router-dom";
 
 function useNodeUtils() {
-  const { getNode, getNodes, getEdges, setNodes, deleteElements } = useReactFlow<Node>();
+  const { save } = useFlow();
+  const { flowId } = useParams();
+  const { getNode, getNodes, getEdges, setNodes, deleteElements, getViewport } =
+    useReactFlow<Node>();
 
   const onNodeDelete = (id: string) => {
-    setNodes(prevNodes => prevNodes.filter(node => node.id !== id));
+    setNodes(prevNodes => {
+      const updatedNodes = prevNodes.filter(node => node.id !== id);
+
+      toast.promise(
+        save.mutateAsync({
+          nodes: updatedNodes,
+          edges: getEdges(),
+          viewport: getViewport(),
+          flowId: flowId!,
+        }),
+        {
+          loading: "Salvataggio in corso...",
+          success: ({ message }) => message,
+          error: ({ message }) => `Errore salvataggio racconto (${message})`,
+        }
+      );
+
+      return updatedNodes;
+    });
     deleteElements({
       nodes: [
         {
@@ -31,7 +54,25 @@ function useNodeUtils() {
     };
 
     toast.success("Pagine create", { duration: 3000 });
-    setNodes(nds => nds.concat(newNode));
+    setNodes(nds => {
+      const updatedNodes = nds.concat(newNode);
+
+      toast.promise(
+        save.mutateAsync({
+          nodes: updatedNodes,
+          edges: getEdges(),
+          viewport: getViewport(),
+          flowId: flowId!,
+        }),
+        {
+          loading: "Salvataggio in corso...",
+          success: ({ message }) => message,
+          error: ({ message }) => `Errore salvataggio racconto (${message})`,
+        }
+      );
+
+      return updatedNodes;
+    });
   };
   const isNodeUnlinked = (id: string) => {
     const nodes = getNodes();
