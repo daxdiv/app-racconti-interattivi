d4232c652d32b7abca9edf86466b2cd0afb6089a
daxdiv <95906740+daxdiv@users.noreply.github.com>
2024-10-29 15:48:45 +0100

    feat(backend): display default background and audio if none of them exist

--- backend/src/constants/index.ts
+++ backend/src/constants/index.ts
@@ -7,3 +7,7 @@ export const MAX_CONTINUE_OPTION_LENGTH = 15;
 export const MIN_USERNAME_LENGTH = 5;
 export const MIN_PASSWORD_LENGTH = 8;
 export const JWT_EXPIRES_IN = 24 * 60 * 60 * 1000; // NOTE: 1 day
+export const DEFAULT_BACKGROUND_URL =
+  "https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80";
+export const DEFAULT_AUDIO_URL =
+  "https://www.voanews.com/embed/player/0/6148249.html?type=audio";
